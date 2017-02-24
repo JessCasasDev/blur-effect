83cb3e1c996d958212f21523a8f81b8784910767
jlcasasg <jlcasasg@unal.edu.co>
2017-02-24 09:44:22 -0500

    gaussian blur-effect

--- Git/blur-effect/blur-effect.c
+++ Git/blur-effect/blur-effect.c
@@ -8,7 +8,7 @@
 
 
 void gaussian_matrix(float ** matrix, int kernel){
-    float sigma = 1.5;
+    float sigma = 5.7;
 /*creamos los valores de las posiciones de la matriz
 //estás posiciones son cartesianas y el punto (0,0) es el centro de la matriz
 //comienza en (-kernel/2,kernel/2) 
@@ -38,7 +38,7 @@ void gaussian_matrix(float ** matrix, int kernel){
         for(int j=0; j<kernel; j++){
             //Para que la suma de la matriz de 1 la normalizamos
             matrix[i][j] = matrix[i][j] / sum;
-            //printf("%.5f ",matrix[i][j]);
+          //  printf("%.5f ",matrix[i][j]);
         }
         //printf("\n");
     }
@@ -47,8 +47,7 @@ void gaussian_matrix(float ** matrix, int kernel){
 
 int main(){
     printf("Starting with opencv\n");
-    int kernel = 4;
-    printf("%d", kernel/2);
+    int kernel = 17;
     float** gaussian;
     gaussian = (float**) malloc(sizeof(float) * kernel);
     //Creamos el espacio en memoria de la matriz gaussiana
@@ -71,98 +70,31 @@ int main(){
    //  printf("%d\n", CV_IMAGE_ELEM(img, uchar, 10, (90*3)+1));    
    // cvWaitKey(0);
    
-   int k = kernel/2;
-   for(int i=0; i<img_height; i++){       
-       for (int j=0; j<img_width; j++){
-           CvScalar p;
-           double blue=0, red=0, green=0;
-           int neighbours = 0;
-           CvScalar s;
-           for(int x=i-k; x<i+k; x++){
-               for(int y=j-k; y<j+k; y++){
-                   if(x>=0 && x<img_height && y>=0 && y<img_width){
-                    s = cvGet2D(img,x,y);
-                    blue += s.val[0];
-                    green += s.val[1];
-                    red += s.val[2];
-                   
-                    neighbours++;
-                       
-                }
-               }      
+    int k = kernel/2;
+    for(int i=k; i<img_height-k; i++){       
+        for (int j=k; j<img_width-k; j++){
+            CvScalar p;
+            double blue=0, red=0, green=0;
+            int neighbours = 0;
+            CvScalar s;
+            for(int x=-k; x<k; x++){
+                for(int y=-k; y<k; y++){
+                    s = cvGet2D(img,i+x,j+y);
+                    blue += s.val[0]*gaussian[y+k][x+k];
+                    green += s.val[1]*gaussian[y+k][x+k];
+                    red += s.val[2]*gaussian[y+k][x+k];
+                    
+                }      
             }
-            p.val[0] = blue/neighbours;
-            p.val[1] = green/neighbours;
-            p.val[2] = red/neighbours;
+            p.val[0] = blue;
+            p.val[1] = green;
+            p.val[2] = red;
             cvSet2D(result,i,j,p);
         }   
     }
    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   /*
-   
-   // cvReleaseImage(&img);
-   
-    for (int i = k; i<img_width-k;i++){
-        for(int j = k; j<img_height-k; j++){
-            float red=0.0, green=0.0, blue=0.0;
-            uchar *b, *g, *r;    
-            for (int g_i = -k; g_i < k; g_i++){
-                for (int g_j = -k; g_j < k; g_j++){
-                    b = &pixels[(i+g_i)*step+(j+g_j)*channels+0];
-                    g = &pixels[(i+g_i)*step+(j+g_j)*channels+1];
-                    r = &pixels[(i+g_i)*step+(j+g_j)*channels+2];
-                    blue += b * &pixels[(i)*step+(j)*channels+0];
-                    green += g * &pixels[(i)*step+(j)*channels+0];
-                    red += r * &pixels[(i)*step+(j)*channels+0];
-                }
-            }
-            ptr[ i * step +j*channels + 0]= blue;
-            ptr[ i * step +j*channels + 1]= green;
-            ptr[i*step +j*channels + 2]= red;
-        }
-    }*/
-    cvNamedWindow("opencvtest",CV_WINDOW_AUTOSIZE);
-    cvShowImage("opencvtest",result);
-   //  printf("%d\n", CV_IMAGE_ELEM(img, uchar, 10, (90*3)+1));    
-    cvWaitKey(0);
-    //Creamos el espacio en memoria de las matrices a usar
-    
- 
-//Creamos tres matrices para separar los colores RGB de cada pixel de la imagen, luego a cada una de esas matrices se le realiza la multiplicación con la matriz de gauss. Para finalmente unir los colores RGb en la nueva imagen
-/* for( row = 0; row < img->height; row++ ){
-   for ( col = 0; col < img->width; col++ ){
-     b = CV_IMAGE_ELEM( img, uchar, row, col * 3 ); 
-     mblue[row][col]=b;
-     g = CV_IMAGE_ELEM( img, uchar, row, col * 3 + 1 );
-     mgreen[row][col]=g;
-     r = CV_IMAGE_ELEM( img, uchar, row, col * 3 + 2 );
-     mred[row][col]=r;
-     //printf("%.d,%.d,%.d\t ", b,g,r);
-     //printf("%c\n", CV_IMAGE_ELEM( img, uchar, row, col * 3 ) );
-   }
- }*/
- 
- //gaussian_matrix(gaussian, kernel);
- //
+    cvNamedWindow("Image Filtered",CV_WINDOW_AUTOSIZE);
+    cvShowImage("Image Filtered",result);
+    cvWaitKey(0);   
     return 0;
 }
